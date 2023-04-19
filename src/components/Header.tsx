94786a02da9455b516171ce2fd48ff8c589a9e58
NathanaelBlackbourn <nathanaelblackbourn@gmail.com>
2023-04-19 16:41:36 +0200

    Scrolling header reactivated.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,80 +5,83 @@ import {
   Container,
   IconButton,
   Link,
+  Slide,
   Typography,
+  useScrollTrigger,
 } from '@mui/material';
+import { FC, ReactElement } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 import { theme } from '../theme';
 import CartIcon from './CartIcon';
 
-// interface Props {
-//   children: ReactElement;
-// }
+interface Props {
+  children: ReactElement;
+}
 
-// // Define the HideOnScroll component as a function component
-// const HideOnScroll: FC<Props> = ({ children }) => {
-//   // useScrollTrigger returns a boolean value indicating whether the user has scrolled down the page
-//   const trigger = useScrollTrigger();
+// Define the HideOnScroll component as a function component
+const HideOnScroll: FC<Props> = ({ children }) => {
+  // useScrollTrigger returns a boolean value indicating whether the user has scrolled down the page
+  const trigger = useScrollTrigger();
 
-//   // We use the Slide component to add a slide animation to the AppBar
-//   // The 'appear' prop determines whether the component should animate on first render
-//   // The 'direction' prop determines the direction of the slide animation
-//   // The 'in' prop determines whether the component should be visible or not
-//   return (
-//     <Slide
-//       appear={false}
-//       direction="down"
-//       in={!trigger || !focus}
-//     >
-//       {children}
-//     </Slide>
-//   );
-// };
+  // We use the Slide component to add a slide animation to the AppBar
+  // The 'appear' prop determines whether the component should animate on first render
+  // The 'direction' prop determines the direction of the slide animation
+  // The 'in' prop determines whether the component should be visible or not
+  return (
+    <Slide
+      appear={false}
+      direction="down"
+      in={!trigger || !focus}
+    >
+      {children}
+    </Slide>
+  );
+};
 
 export default function Header() {
   return (
-    // <HideOnScroll>
-    <AppBar sx={styledAppBar}>
-      <Container sx={styledContainer}>
-        <Link
-          component={RouterLink}
-          to="/"
-          sx={styledLink}
-        >
-          <Typography
-            variant="h5"
-            sx={styledLogo}
-          >
-            Piece by Piece
-          </Typography>
-        </Link>
-        <Box>
+    <HideOnScroll>
+      <AppBar sx={styledAppBar}>
+        <Container sx={styledContainer}>
           <Link
             component={RouterLink}
-            to="/checkout"
+            to="/"
+            sx={styledLink}
           >
-            <IconButton
-              data-cy="cart-link"
-              aria-label="cart"
+            <Typography
+              variant="h5"
+              sx={styledLogo}
             >
-              <CartIcon />
-            </IconButton>
+              Piece by Piece
+            </Typography>
           </Link>
-          <Link
-            component={RouterLink}
-            to="/admin"
-          >
-            <IconButton
-              aria-label="admin"
-              data-cy="admin-link"
+          <Box>
+            <Link
+              component={RouterLink}
+              to="/checkout"
             >
-              <PersonOutlineOutlinedIcon sx={styledPersonOutlined} />
-            </IconButton>
-          </Link>
-        </Box>
-      </Container>
-    </AppBar>
-    // </HideOnScroll>
+              <IconButton
+                data-cy="cart-link"
+                aria-label="cart"
+              >
+                <CartIcon />
+              </IconButton>
+            </Link>
+            <Link
+              component={RouterLink}
+              to="/admin"
+            >
+              <IconButton
+                aria-label="admin"
+                data-cy="admin-link"
+              >
+                <PersonOutlineOutlinedIcon sx={styledPersonOutlined} />
+              </IconButton>
+            </Link>
+          </Box>
+        </Container>
+      </AppBar>
+    </HideOnScroll>
   );
 }
 
